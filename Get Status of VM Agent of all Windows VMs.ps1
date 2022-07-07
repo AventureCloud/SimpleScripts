7c3347d0ddb5034dcea76829cba4c07e9ef533e9
AventureCloud <93791265+AventureCloud@users.noreply.github.com>
2022-07-07 09:53:31 -0400

    Update Get Status of VM Agent of all Windows VMs.ps1

--- Get Status of VM Agent of all Windows VMs.ps1
+++ Get Status of VM Agent of all Windows VMs.ps1
@@ -1,5 +1,4 @@
 $vms = Get-AzVM
-
 foreach ($vm in $vms) {
     $agent = $vm | Select -ExpandProperty OSProfile | Select -ExpandProperty Windowsconfiguration | Select ProvisionVMAgent
     Write-Host $vm.Name $agent.ProvisionVMAgent
